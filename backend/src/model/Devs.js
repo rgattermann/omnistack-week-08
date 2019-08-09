6cda3419e114c05267c66ede3a75c1836404aca1
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2019-08-09 07:43:07 -0300

    Add mongodb connection

---
+++ backend/src/model/Devs.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+
+const DevSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-dg0xs.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
 server.use(express.json);
 server.use(routes);
 
